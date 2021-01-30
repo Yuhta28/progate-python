b761fb324ad9f0618ef99edb78053365c8564fe7
Yuhta <yuta_mori@outlook.jp>
2021-01-30 11:59:44 +0900

    fix lesson2 script

--- lesson2/script.py
+++ lesson2/script.py
@@ -6,15 +6,17 @@ for item_name in items:
 
     print(item_name + "は1個" + str(items[item_name]) + "円です")
 
-    input_count = input("購入する" + item_name + "の個数を入力してください：")
-    print("購入する" + item_name + "の個数は" + input_count + "個です")
+    #input_count = input("購入する" + item_name + "の個数を入力してください：")
+    print("購入する" + item_name + "の個数を入力してください：3")
+    int_count=3
+    print("購入する" + item_name + "の個数は" + int_count + "個です")
 
-    count = int(input_count)
+    count = int(int_count)
     total_price = items[item_name] * count
     print("支払い金額は" + str(total_price) + "円です")
 
     if money >= total_price:
-        print(item_name + "を" + input_count + "個買いました")
+        print(item_name + "を" + int_count + "個買いました")
         money -= total_price
         if money == 0:
             print("財布が空になりました")
