65ad7907e871ac71b735aa0570067777d33f811d
Yuhta <yuta_mori@outlook.jp>
2021-02-05 20:19:11 +0900

    fix lesson3

--- lesson3/script.py
+++ lesson3/script.py
@@ -1,8 +1,8 @@
 def validate(hand):
     if hand > 2 or hand < 0:
         return False
-    else:
-        return True
+
+    return True
 
 
 def print_hand(hand,name="ゲスト"):
@@ -10,9 +10,12 @@ def print_hand(hand,name="ゲスト"):
 
     print(name + "は" + hands[hand] + "を出しました")
 print("じゃんけんを始めます")
-player_name = input("名前を入力してください：")
+#player_name = input("名前を入力してください：")
+print("名前を入力してください:Test")
+player_name = "Test"
 print("何を出しますか？（0: グー, 1: チョキ, 2: パー）")
-player_hand = int(input("数字で入力してください："))
+#player_hand = int(input("数字で入力してください："))
+player_hand = "0"
 
 if player_name == "":
     print_hand(player_hand)
