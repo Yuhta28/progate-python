ffda6efa1939d309f76715b91b97f0492b34bf9b
Yuhta <yuta_mori@outlook.jp>
2021-02-07 14:56:10 +0900

    fix lesson4

--- lesson4/script.py
+++ lesson4/script.py
@@ -1,21 +1,18 @@
 class MenuItem:
     def info(self):
-        print("メニューの名前と値段が表示されます")
+        print(self.name + ": ¥" + str(self.price))
 
 menu_item1 = MenuItem()
 menu_item1.name = "サンドイッチ"
-print(menu_item1.name)
 
 menu_item1.price = 500
-print(menu_item1.price)
 
 menu_item1.info()
 
 menu_item2 = MenuItem()
 menu_item2.name = "チョコケーキ"
-print(menu_item2.name)
+
 
 menu_item2.price = 400
-print(menu_item2.price)
 
 menu_item2.info()
