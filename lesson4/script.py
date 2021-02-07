02db1c56381ee56196072453a4ee1d0f1f7307df
Yuhta <yuta_mori@outlook.jp>
2021-02-07 16:08:08 +0900

    fix lesson4

--- lesson4/script.py
+++ lesson4/script.py
@@ -1,9 +1,23 @@
+import random
 from menu_item import MenuItem
 
 menu_item1 = MenuItem("サンドイッチ",500)
+menu_item2 = MenuItem("チョコケーキ",400)
+menu_item3 = MenuItem("コーヒー",300)
+menu_item4 = MenuItem("オレンジジュース",200)
 
-print(menu_item1.info())
+menu_items = [menu_item1,menu_item2,menu_item3,menu_item4]
 
-result = menu_item1.get_total_price(4)
+index = 0
 
-print("合計は" + str(result) + "円です")
+for item in menu_items:
+    print(str(index) + ". " + item.info())
+    index += 1
+
+print("--------------------")
+
+#order = int(input("メニューの番号を入力してください: ")) 
+order = random.randint(0,3)
+selected_menu = menu_items[order]
+
+print("選択されたメニュー：　" + selected_menu.name)
