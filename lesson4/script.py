f846c6570e1314a7908657b667a0920cf0c5ad65
Yuhta <yuta_mori@outlook.jp>
2021-02-07 15:09:32 +0900

    fix lesson4

--- lesson4/script.py
+++ lesson4/script.py
@@ -2,6 +2,10 @@ class MenuItem:
     def info(self):
         return self.name + ": ¥" + str(self.price)
 
+    def get_total_price(self, count):
+        total_price = self.price * count
+        return total_price
+
 menu_item1 = MenuItem()
 menu_item1.name = "サンドイッチ"
 
@@ -9,10 +13,6 @@ menu_item1.price = 500
 
 print(menu_item1.info())
 
-menu_item2 = MenuItem()
-menu_item2.name = "チョコケーキ"
+result = menu_item1.get_total_price(4)
 
-
-menu_item2.price = 400
-
-print(menu_item2.info())
+print("合計は" + str(result) + "円です")
