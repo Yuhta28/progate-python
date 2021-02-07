950c9af8b07b6404a4f059aea0a3ebe73c1c671b
Yuhta <yuta_mori@outlook.jp>
2021-02-07 17:44:24 +0900

    fix config

--- lesson5/script.py
+++ lesson5/script.py
@@ -2,11 +2,35 @@ from food import Food
 from drink import Drink
 
 food1 = Food("サンドイッチ",500,330)
+food2 = Food("チョコケーキ",400,450)
+food3 = Food("シュークリーム",200,180)
 
-
-print(food1.info())
+foods = [food1,food2,food3]
 
 drink1 = Drink("コーヒー",300,180)
+drink2 = Drink("オレンジジュース",200,350)
+drink3 = Drink("エスプレッソ",500,30)
 
+drinks = [drink1,drink2,drink3]
 
-print(drink1.info())
+print("食べ物メニュー")
+index = 0
+for food in foods:
+    print(str(index) + ". " + food.info())
+    index += 1
+
+print ("飲み物メニュー")
+index = 0
+for drink in drinks:
+    print(str(index) + ". " + drink.info())
+    index += 1
+
+print("--------------------")
+
+food_order = int(input("食べ物の番号を選択してください: "))
+selected_food = foods[food_order]
+
+drink_order = int(input("飲み物の番号を選択してください: "))
+selected_drink = drinks[drink_order]
+
+count
